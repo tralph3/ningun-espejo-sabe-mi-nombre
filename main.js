2a5a341947109f43f20912b26766cc83cf964473
tralph3 <tomasralph2000@gmail.com>
2020-09-05 21:13:37 -0300

    Added third text support

--- main.js
+++ main.js
@@ -2,28 +2,41 @@ $(document).click(function(event) {
     changeText($(event.target));
 });
 
-function changeText($paragraph){
-    if($paragraph.attr('class') == "front"){
+function changeText($paragraph)
+{
+    if($paragraph.attr('class') == "first")
+    {
         id = $paragraph.attr("id").slice(2);
         $paragraph.fadeOut(200, function(){
-            $paragraph.html(backgroundText[id]);
+            $paragraph.html(secondText[id]);
             $paragraph.fadeIn(200);
-            $paragraph.attr('class', "background");
+            $paragraph.attr('class', "second");
         });
-    }else if($paragraph.attr('class') == "background"){
+    }
+    else if($paragraph.attr('class') == "second")
+    {
         id = $paragraph.attr("id").slice(2);
         $paragraph.fadeOut(200, function(){
-            $paragraph.html(frontText[id]);
+            $paragraph.html(thirdText[id]);
             $paragraph.fadeIn(200);
-            $paragraph.attr('class', "front");
+            $paragraph.attr('class', "third");
+        });
+    }
+    else if($paragraph.attr('class') == "third")
+    {
+        id = $paragraph.attr("id").slice(2);
+        $paragraph.fadeOut(200, function(){
+            $paragraph.html(firstText[id]);
+            $paragraph.fadeIn(200);
+            $paragraph.attr('class', "first");
         });
     }
 }
 
 function initialize(){
-    for(let i = 0; i < frontText.length; i++){
-        $('.mainDiv').append('<p class="front" id="id' + i.toString() + '"></p>')
-        $('#id' + i.toString()).html(frontText[i]);
+    for(let i = 0; i < firstText.length; i++){
+        $('.mainDiv').append('<p class="first" id="id' + i.toString() + '"></p>')
+        $('#id' + i.toString()).html(firstText[i]);
     }
 }
 
