ed9fc34cf80f83ff2c0d3174b7b051795b8c81e1
tralph3 <tomasralph2000@gmail.com>
2020-09-09 22:44:03 -0300

    Added color circles and general improvements

--- main.js
+++ main.js
@@ -1,43 +1,87 @@
-$(document).click(function(event) {
-    changeText($(event.target));
+$(document).click(function(event)
+{
+    clicked($(event.target));
 });
 
-function changeText($paragraph)
+function clicked($element)
 {
-    if($paragraph.attr('class') == "first")
+    if($element.attr('class') == "circle")
     {
-        id = $paragraph.attr("id").slice(2);
-        $paragraph.fadeOut(200, function(){
-            $paragraph.html(secondText[id]);
-            $paragraph.fadeIn(200);
-            $paragraph.attr('class', "second");
+	text = getText($element);
+	$(".textDiv").fadeOut(200, function(){
+	    document.querySelectorAll('[name="text"]').forEach(function($paragraph, index)
+	    {
+		$paragraph.innerHTML = text[index];
+		$paragraph.className = $element.attr("id");
+	    });
+	    $(".textDiv").fadeIn(200);
+	});
+	return;
+    }
+
+    if($element.attr('class') == "first")
+    {
+        id = $element.attr("id").slice(2);
+        $element.fadeOut(200, function(){
+            $element.html(secondText[id]);
+            $element.fadeIn(200);
+            $element.attr('class', "second");
         });
     }
-    else if($paragraph.attr('class') == "second")
+    else if($element.attr('class') == "second")
     {
-        id = $paragraph.attr("id").slice(2);
-        $paragraph.fadeOut(200, function(){
-            $paragraph.html(thirdText[id]);
-            $paragraph.fadeIn(200);
-            $paragraph.attr('class', "third");
+        id = $element.attr("id").slice(2);
+        $element.fadeOut(200, function(){
+            $element.html(thirdText[id]);
+            $element.fadeIn(200);
+            $element.attr('class', "third");
         });
     }
-    else if($paragraph.attr('class') == "third")
+    else if($element.attr('class') == "third")
     {
-        id = $paragraph.attr("id").slice(2);
-        $paragraph.fadeOut(200, function(){
-            $paragraph.html(firstText[id]);
-            $paragraph.fadeIn(200);
-            $paragraph.attr('class', "first");
+        id = $element.attr("id").slice(2);
+        $element.fadeOut(200, function(){
+            $element.html(firstText[id]);
+            $element.fadeIn(200);
+            $element.attr('class', "first");
         });
     }
 }
 
-function initialize(){
-    for(let i = 0; i < firstText.length; i++){
-        $('.mainDiv').append('<p class="first" id="id' + i.toString() + '"></p>')
+function initialize()
+{
+    for(let i = 0; i < firstText.length; i++)
+    {
+        $('.textDiv').append('<p name="text" class="first" id="id' + i.toString() + '"></p>')
         $('#id' + i.toString()).html(firstText[i]);
     }
+
+    showAlert();
+}
+
+function getText($circle)
+{
+    id = $circle.attr("id");
+    if(id == "first")
+    {
+	return firstText;
+    }
+    else if(id == "second")
+    {
+	return secondText;
+    }
+    else if(id == "third")
+    {
+	return thirdText;
+    }
+}
+
+function showAlert()
+{
+    swal({
+	text: "Este cuento es interactivo. Puede hacer click en los párrafos para ver diferentes versiones de la historia, o puede utilizar los círculos de colores para cambiar todos los párrafos a la vez.",
+	icon: "info"
+    })
 }
 
 initialize();
